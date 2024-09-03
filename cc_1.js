a5cceaa8fdc8ef6123da6d42d241e37f0a2722a3
belapiana <borgesi@usf.edu>
2024-09-03 13:35:04 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Anna";
+const employeeID = 546;
+var = isActive = true;
+
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
