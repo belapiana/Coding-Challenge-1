59f2f07970642a0c132b61154c2a67f1c0277e64
belapiana <borgesi@usf.edu>
2024-09-03 13:45:25 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -2,8 +2,17 @@
 
 let employeeName = "Anna";
 const employeeID = 546;
-var = isActive = true;
+var isActive = true;
 
-console.log(employeeName, typeof employeeName);
-console.log(employeeID, typeof employeeID);
-console.log(isActive, typeof isActive);
+console.log(employeeName, typeof (employeeName));
+console.log(employeeID, typeof (employeeID));
+console.log(isActive, typeof (isActive));
+
+// Task 2: Compound Data Types
+
+let products = ["chocolate", "lollipop", "candy"];
+const productsDetails = {name: "chocolate",
+    price: 5.40,
+    inStock: true};
+
+    console.log(products,productsDetails);
